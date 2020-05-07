c8f40ffc46abc12b5e80ce3c50b6dbc6dac5b282
MekkyMayata <okpallannaemeka@gmail.com>
2020-05-07 07:09:14 +0100

    testing

--- test/base_test.js
+++ test/base_test.js
@@ -3,9 +3,6 @@ const index = require("../fix");
 
 describe('tests codebase', () => {
 
-  // it('asserts', () => {
-  //   assert(2 !== 'two');
-  // })
   it('asserts', () => {
     assert(index.tests(2) === 4);
   })
